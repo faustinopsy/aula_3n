dd04b97200a1f411c3cf07cf1e93c12471738dd4
faustinopsy <rodrigohipnose@gmail.com>
2025-02-28 11:53:17 -0300

    aula 3

---
+++ arrays.js
@@ -0,0 +1,15 @@
+//  posições     0     1  
+const fila = [23,45,34,47]
+//metodos para adicionar
+fila.unshift(67)
+fila.push(34)
+console.log(fila)
+
+//metodos para remover
+fila.pop()
+fila.shift()
+console.log(fila)
+
+//recortar parte do array
+const parte = fila.slice(1,3)
+console.log(parte)
---
+++ index.js
@@ -0,0 +1,1 @@
+console.log("usar no terminal node + o arquivo correspondnete")
